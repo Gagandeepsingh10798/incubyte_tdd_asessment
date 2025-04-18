1a9fd3b4541ba8003f852a04b564419f8542e5a8
gagandeepsingh <gagandeep.singh@credencedata.com>
2025-04-18 23:43:36 +0530

    Step 9: Support multiple single-character delimiters

--- tests/test_string_calculator.py
+++ tests/test_string_calculator.py
@@ -34,3 +34,7 @@ def test_negative_numbers_raise_exception():
 def test_delimiter_of_any_length():
     calculator = StringCalculator()
     assert calculator.add("//[***]\n1***2***3") == 6
+
+def test_multiple_delimiters():
+    calculator = StringCalculator()
+    assert calculator.add("//[*][%]\n1*2%3") == 6
